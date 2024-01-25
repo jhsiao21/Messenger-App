befcbf580648757c33be4c96c7424261a3e4d073
Logan <logan.hsiao.jk@gmail.com>
2024-01-25 16:03:14 +0800

    Login & Register ViewController

---
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by LoganMacMini on 2024/1/25.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
+
+}
+
